1ae869e9b258370885e2f601e65ec3b6239cb506
oshri22 <oshritaub@gmail.com>
2020-10-08 11:50:12 +0300

    ..

--- backend/BackEndServer.py
+++ backend/BackEndServer.py
@@ -1,5 +1,6 @@
-from flask import Flask, url_for, render_template , session, request, redirect
+from flask import Flask, session, request
 from markupsafe import escape
+import os
 
 app = Flask(__name__)
 app.secret_key = b''
@@ -26,3 +27,9 @@ def form():
 @app.route('/menu')
 def  menu():
     pass
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='127.0.0.1', port=port)
+    

a439c8c145733d405232b1861aec6893533cc108
gal holtzman <galbuba@gmail.com>
2020-10-10 16:41:23 +0300

    add register

--- backend/database.py
+++ backend/database.py
@@ -13,6 +13,7 @@ class Database():
         :param data_name: the name of the database to connect to 
         """
         self.connection: sqlite3.Connection = sqlite3.connect(data_name, check_same_thread=False)
+        self.name = data_name
         self.cursor: sqlite3.Cursor = self.connection.cursor()
         self.sql_lock = Lock()
 
@@ -44,10 +45,55 @@ class Database():
         self.sql_lock.release()
         return json.dumps({"name": "error", "id": -1, "money": -999, "login": False})
 
+    def register(self, params: str) -> str:
+        params = json.loads(params)
+        self.sql_lock.acquire()
+        query: str = "INSERT INTO users VALUES(?, ?, ?, NULL)"
+        check_query: str = "SELECT * FROM users WHERE name = ? LIMIT 1" 
+        # Check if the user already exists
+
+        #if it is return login error      
+        users = self.cursor.execute(check_query, (params["name"],))
+        has_user = False
+        for user in users:
+            has_user = True
+        
+
+        # Reset the connection
+        self.sql_lock.release()
+        self.cursor.close()
+        self.connection.close()
+        self.connection = sqlite3.connect(self.name, check_same_thread=False)
+        self.cursor = self.connection.cursor()
+        self.sql_lock.acquire()
+
+        if has_user:
+            self.sql_lock.release()
+            return json.dumps({"name": "error", "id": -1, "money": -999, "login": False})
+        
+        else:
+            #Create a new user and return its stats
+            try:
+                self.cursor.execute(query,(params["name"], params["password"], params["money"]))
+                self.connection.commit()
+                self.sql_lock.release()
+                user = self.user_login(params["name"], params["password"])
+
+                return user
+            except sqlite3.OperationalError as e:
+                print(e)
+
+            self.sql_lock.release()
+            return json.dumps({"name": "error", "id": -1, "money": -999, "login": False})
+
+
 
 def main():
+    print("started")
     data = Database("data.db")
-    print(data.user_login('gal"--', "sesam"))
+    
+    print(data.register('{"name": "galol66", "password": "sesame", "money": 100}'))
+    print(data.user_login("galol", "sesame"))
 
 
 if __name__ == "__main__":
